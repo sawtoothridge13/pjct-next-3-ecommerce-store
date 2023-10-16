07a65b11871d170bf5e1d9fb713ced6f0d9c5494
Doug Andrews <sawtoothridge13@gmail.com>
2023-10-16 15:38:43 +0200

    Reconfigure config.mjs and ley.config.mjs

--- ley.config.js
+++ ley.config.js
@@ -1,32 +1,9 @@
-const options = {};
+import { setEnvironmentVariables } from './util/config.mjs';
 
-if (process.env.DATABASE_URL) {
-  options.ssl = {
-    rejectUnauthorized: false,
-    require: true,
-  };
+setEnvironmentVariables();
 
-  // Parse the DATABASE_URL to extract necessary connection parameters
-  const url = new URL(process.env.DATABASE_URL);
-  process.env.PGHOST = url.host;
-  process.env.PGUSER = url.username;
-  process.env.PGDATABASE = url.pathname.slice(1); // Remove leading slash from pathname
-  process.env.PGPASSWORD = url.password;
-  process.env.DATABASE_URL = process.env.DATABASE_URL; // Optional: Set DATABASE_URL as well
-}
-
-module.exports = {
-  ...options,
-  // Set environment variables for production, preview, and development environments
-  ...(process.env.NOW_REGION === 'dev1'
-    ? {
-        env: {
-          PGHOST: process.env.PGHOST,
-          PGUSER: process.env.PGUSER,
-          PGDATABASE: process.env.PGDATABASE,
-          PGPASSWORD: process.env.PGPASSWORD,
-          DATABASE_URL: process.env.DATABASE_URL,
-        },
-      }
-    : {}),
+const options = {
+  ssl: Boolean(process.env.POSTGRES_URL),
 };
+
+export default options;
