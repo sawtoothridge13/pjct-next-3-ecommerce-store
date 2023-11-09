71f8c531981d1c0ce5dfd1bab73583a5abef9db7
Doug Andrews <sawtoothridge13@gmail.com>
2023-11-09 10:47:33 +0100

    Remove test functions

--- util/__tests__/carttotalprice.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@jest/globals';
-import { cartSum } from '../testfunctions/cartsum';
-
-test('update quantity of cart item', () => {
-  const total = cartSum();
-  expect(total).toStrictEqual(120);
-  expect(typeof total).toBe('number');
-});
